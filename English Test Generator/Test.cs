e65db2e9848174ff1eae01c7845b852aca31ed69
Simo Aleksandrov <simo3003@me.com>
2018-02-25 16:33:09 +0200

    Remove redundant message boxes

--- English Test Generator/Test.cs
+++ English Test Generator/Test.cs
@@ -19,10 +19,6 @@ namespace English_Test_Generator
                 string[] splitByAsterisk = splitByNewLine[i].Split(new string[] {"*"}, StringSplitOptions.RemoveEmptyEntries);
                 Form1.test_WordsAndTypes.Add(splitByAsterisk[0], splitByAsterisk[1]);
             }
-            foreach (KeyValuePair<string, string> entry in Form1.test_WordsAndTypes)
-            {
-                MessageBox.Show(entry.Key + "\n" + entry.Value);
-            }
         }
         public static string Generate(string test_Type, int test_ExcerciseAmount, string test_Name, Dictionary<string, string> test_Words, string region)
         {
@@ -42,10 +38,6 @@ namespace English_Test_Generator
                         break;
                 }
             }
-            foreach (var exercise in exercises)
-            {
-                MessageBox.Show(exercise);
-            }
             return "";
         }
         public static string Read(string source)
@@ -59,7 +51,6 @@ namespace English_Test_Generator
                     filteredSource += lines[i] + "\n";
                 }
             }
-            MessageBox.Show(filteredSource);
             return filteredSource;
         }
     }
