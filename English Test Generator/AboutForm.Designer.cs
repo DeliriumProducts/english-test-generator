e73974d1757e08c22a577a3aa8b61c1688c83517
Simo Aleksandrov <simo3003@me.com>
2018-03-09 19:14:45 +0200

    Change StartPosition property to Center of Form1 and the About Form

--- English Test Generator/AboutForm.Designer.cs
+++ English Test Generator/AboutForm.Designer.cs
@@ -380,6 +380,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "AboutForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Load += new System.EventHandler(this.AboutForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
--- English Test Generator/Form1.Designer.cs
+++ English Test Generator/Form1.Designer.cs
@@ -1238,6 +1238,7 @@
             this.KeyPreview = true;
             this.MaximizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "English Test Generator";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
